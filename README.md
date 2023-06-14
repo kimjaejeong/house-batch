53ae163b1d360911ca0909b30c6761297475746f
kimjaejeong <dyddkdlel33@naver.com>
2023-06-14 18:25:53 +0900

    CompositJobParameterValidator 추가, api 호출 코드 추가

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ house-batch를 위한 API입니다.
   - https://www.data.go.kr/iim/api/selectAPIAcountView.do
     - url: End Point
     - serviceKey: 일반 인증키(Encoding)
+- 실행
+```
+- --spring.batch.job.names=lawdInsertJob -filePath=LAWD_CODE_SAMPLE.txt
+- --spring.batch.job.names=aptDealInsertJob -lawdCd=41135 -yearMonth=2021-07
+```
--- src/main/java/com/example/housebatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/com/example/housebatch/job/apt/AptDealInsertJobConfig.java
@@ -2,23 +2,32 @@ package com.example.housebatch.job.apt;
 
 import com.example.housebatch.adapter.ApartmentApiResource;
 import com.example.housebatch.core.dto.AptDealDto;
+import com.example.housebatch.core.repository.LawdRepository;
+import com.example.housebatch.job.validator.LawdCdParameterValidator;
+import com.example.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import java.time.YearMonth;
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,13 +37,48 @@ public class AptDealInsertJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     private final ApartmentApiResource apartmentApiResource;
+    private final LawdRepository lawdRepository;
 
     @Bean
-    public Job apiDealInsertJob(Step aptDealInsertStep) {
+    public Job aptDealInsertJob(
+            Step guLawdCdStep
+//            Step aptDealInsertStep
+    ) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
-                .start(aptDealInsertStep)
+//                .validator(aptDealJobParameterValidator())
+                .start(guLawdCdStep)
+//                .start(aptDealInsertStep)
                 .build();
+    }
+
+
+    // validation을 여러개 하기 위함
+    private JobParametersValidator aptDealJobParameterValidator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+        validator.setValidators(Arrays.asList(
+                new YearMonthParameterValidator(),
+                new LawdCdParameterValidator()
+        ));
+        return validator;
+    }
+
+    @JobScope
+    @Bean
+    public Step guLawdCdStep(Tasklet guLawdCdTasklet) {
+        return stepBuilderFactory.get("guLawdCdStep")
+                .tasklet(guLawdCdTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet guLawdCdTasklet() {
+        return (stepContribution, chunkContext) -> {
+            lawdRepository.findDistinctGuLawdCd()
+                    .forEach(System.out::println);
+            return RepeatStatus.FINISHED;
+        };
     }
 
     @JobScope
@@ -53,11 +97,13 @@ public class AptDealInsertJobConfig {
     @StepScope
     @Bean
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
+            @Value("#{jobParameters['yearMonth']}") String yearMonth,
+            @Value("#{jobParameters['lawdCd']}") String lawdCd,
             Jaxb2Marshaller aptDealDtoMarshaller
     ) {
         return new StaxEventItemReaderBuilder<AptDealDto>()
                 .name("aptDealResourceReader")
-                .resource(apartmentApiResource.getResource("41135", YearMonth.of(2021,7)))
+                .resource(apartmentApiResource.getResource(lawdCd, YearMonth.parse(yearMonth)))
                 .addFragmentRootElements("item")
                 .unmarshaller(aptDealDtoMarshaller)
                 .build();
